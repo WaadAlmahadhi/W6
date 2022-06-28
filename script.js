53dabe2cc814b0d4a7e2be2b9ddd6425b1a29640
WaadAlmahadhi <wa3dalwad3y@gmail.com>
2022-06-28 23:50:38 +0300

    add all file

---
+++ script.js
@@ -0,0 +1,18 @@
+function larg()
+{
+    var element=document.getElementById("pr1");
+    var element2=document.getElementById("yelist");
+    var element3=document.getElementById("leftlist");
+    element.style.fontSize = "x-large";
+    element2.style.fontSize = "x-large";
+    element3.style.fontSize = "large";
+}
+function normal()
+{
+    var element=document.getElementById("pr1");
+    var element2=document.getElementById("yelist");
+    var element3=document.getElementById("leftlist");
+    element.style.fontSize = "medium";
+    element2.style.fontSize = "medium";
+    element3.style.fontSize = "medium";
+}
